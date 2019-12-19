965fbc5684bbf8f6a25f2f254e8075f24c52bb99
Vladislav Postolachi <postolachi.vlad@gmail.com>
2019-12-19 20:55:56 +0100

    add a photo colum in Cocktail table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_101527) do
+ActiveRecord::Schema.define(version: 2019_12_19_195324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_12_14_101527) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "doses", force: :cascade do |t|
